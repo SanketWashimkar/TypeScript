e44463232c72fc79058f54e2b4107cd155e1e424
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-29 15:13:07 +0530

    main:7th commit

---
+++ typebasic/typeVar/typeVar.ts
@@ -0,0 +1,61 @@
+const typeVar = () =>{
+
+    //premitiv type
+    const string1:string = "test string";
+    const number:number = 7;
+    const booleanType:boolean = false;
+    const undifineType:undefined = undefined;
+    const nullType:null = null;
+    
+    //complex type
+    const emptyArr:[] = [];
+    
+    const arr1:string[] = ['sanket'];
+    
+    const numArr:number[] = [1,2,3,4,5,6,7,8];
+    
+    const stingAndnumberArr: (string | number)[] = ['abc', 5];
+    
+    type sub = 'sub';
+    type add = 'add';
+    type mul = 'mul';
+    type divi = 'divi';
+    
+    const arrCustoms:(add | sub | mul | divi)[] = ['add','mul','divi','sub'];
+    
+    // objects
+    //Should not do this
+    
+    let obj1:{} = {};
+    
+    obj1 = 'string';
+    obj1 = 123;
+    
+    //should not do this
+    
+    let objUser:
+    {
+    name: string,
+    age: number,
+    city?:string,
+    mob:number | undefined,
+    statusOfadd: sub | mul | divi | add,
+    nullOrString: string | null
+    
+    
+    }
+    
+    ={
+    name:'Sanket',
+    age:25,
+    city: 'Nagpur',
+    mob:undefined,
+    statusOfadd:'sub',
+    nullOrString:null
+    
+    };
+    
+    //objUser.test1 = 'test' not possible due to type checking
+    
+    }
+    typeVar();
