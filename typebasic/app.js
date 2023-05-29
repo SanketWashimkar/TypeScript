c5f289ea4501f2e6bb641033230d66c413eca9b0
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-29 12:28:28 +0530

    my first commit

---
+++ typebasic/app.js
@@ -0,0 +1,7 @@
+console.log("this is a ts file");
+var fun1 = function () {
+    var a = 10;
+    var b = 12;
+    console.log(a + b);
+};
+fun1();
---
+++ typebasic/app.ts
@@ -0,0 +1,8 @@
+console.log("this is a ts file");
+const fun1 = () => {
+const a = 10;
+const b = 12;
+
+console.log(a+b);
+}
+fun1();
