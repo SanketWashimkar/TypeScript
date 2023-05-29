c4f7db621e80c9ff5f40f6ce6d7758246c2eb6a2
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-29 12:49:36 +0530

    main:2nd commit

--- typebasic/app.js
+++ typebasic/app.js
@@ -1,7 +1,13 @@
 console.log("this is a ts file");
 var fun1 = function () {
     var a = 10;
-    var b = 12;
-    console.log(a + b);
+    var b = '12';
+    var add = a + b;
+    console.log(add);
+    return add;
 };
-fun1();
+// const returnAdd:string = fun1();
+// console.log('returnAdd', returnAdd);
+var addFun = function (a, b) { return a + parseInt(b); };
+var addfunReturn = addFun(1, '2').toString();
+console.log(addfunReturn);
